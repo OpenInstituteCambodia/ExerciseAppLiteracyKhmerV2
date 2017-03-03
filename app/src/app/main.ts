9ab7218ce94cef6f3ff4d7a60a9825585c92cb24
Socheat Sok <socheatsok78@gmail.com>
2017-03-03 23:21:50 +0700

    add angular2 enableProdMode() for later use

--- app/src/app/main.ts
+++ app/src/app/main.ts
@@ -1,5 +1,9 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+
+// Angular 2 is running in the development mode. Call enableProdMode() to enable the production mode.
+// enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
