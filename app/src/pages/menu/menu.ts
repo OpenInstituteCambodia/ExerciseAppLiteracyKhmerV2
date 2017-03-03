0a089318c1c00a0bf78b38c407689db24b7aec30
Socheat Sok <socheatsok78@gmail.com>
2017-03-03 18:05:34 +0700

    add chapter title to header navbar:

--- app/src/pages/menu/menu.ts
+++ app/src/pages/menu/menu.ts
@@ -13,24 +13,32 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class MenuPage {
   public menuID;
+  public menuTitle = 'ជីវវិទ្យា​ ថ្នាក់​ទី​១២';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     console.log(this.navParams.get('menuID'));
+
     if (typeof this.navParams.get('menuID') == 'undefined') {
       this.menuID = 'root';
     } else {
       this.menuID = this.navParams.get('menuID');
     }
+
+    if (typeof this.navParams.get('title') != 'undefined') {
+      this.menuTitle = this.navParams.get('title');
+    }
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
   }
 
-  public menu(id) {
+  public menu(id, title) {
     this.navCtrl.push(
       MenuPage, {
-        menuID: id
+        menuID: id,
+        title: title
     });
   }
 
