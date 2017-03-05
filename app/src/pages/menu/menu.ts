fc4553fed3dc27061bae3bff43e239d79f20d07b
Socheat Sok <socheatsok78@gmail.com>
2017-03-05 14:00:25 +0700

    remove comment of ionic

--- app/src/pages/menu/menu.ts
+++ app/src/pages/menu/menu.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { UnitPage } from '../unit/unit';
 
-/*
-  Generated class for the Menu page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html'
