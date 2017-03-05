07915176bbda6f1632bf33bc380c2bc88b6909b6
Socheat Sok <socheatsok78@gmail.com>
2017-03-05 23:48:29 +0700

    add schemas: [ NO_ERRORS_SCHEMA ]

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule, ErrorHandler, NO_ERRORS_SCHEMA } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { MenuPage } from '../pages/menu/menu';
@@ -19,6 +19,7 @@ import { UnitPage } from '../pages/unit/unit';
     MenuPage,
     UnitPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule {}
