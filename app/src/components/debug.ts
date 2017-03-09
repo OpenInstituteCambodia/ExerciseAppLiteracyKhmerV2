34c2f620d5f9fe1b909c02412a4bbff137b0a91c
Socheat Sok <socheatsok78@gmail.com>
2017-03-09 09:32:03 +0700

    debugging, log history to localStorage

--- app/src/components/debug.ts
+++ app/src/components/debug.ts
@@ -11,7 +11,7 @@ import { NavController, NavParams } from 'ionic-angular';
   selector: 'page-debug',
   template: `
     <ion-header>
-      <ion-navbar hideBackButton>
+      <ion-navbar hideBackButton color="danger">
         <button ion-button icon-only menuToggle navPop>
           <ion-icon name="close"></ion-icon>
         </button>
@@ -19,17 +19,79 @@ import { NavController, NavParams } from 'ionic-angular';
       </ion-navbar>
     </ion-header>
 
-    <ion-content padding>
-    
+    <ion-content>
+      <div *ngIf="debugInterface == 'menu'">
+        <ion-item-group>
+          <ion-item-divider color="light">Route</ion-item-divider>
+          <ion-item>
+            <ion-label floating>Unit ID</ion-label>
+            <ion-input #routeID type="text"></ion-input>
+          </ion-item>
+          <div padding>
+            <button ion-button block (click)="route(routeID)">Go</button>
+          </div>
+        </ion-item-group>
+        <ion-item-group>
+          <ion-item-divider color="light">History</ion-item-divider>
+          <ion-item (click)="route('C1L1Q1')">
+            C1L1Q1
+          </ion-item>
+        </ion-item-group>
+      </div>
+
+      <ion-item-group *ngIf="debugInterface == 'unit'">
+        <ion-item-divider color="light">
+          <h1>Unit ID: {{content['unit_id']}}</h1>
+        </ion-item-divider>
+          <ion-item>
+            Audio 1: {{content['audio_1']}}
+          </ion-item>
+          <ion-item>
+            Audio 2: {{content['audio_2']}}
+          </ion-item>
+
+        <ion-item-divider color="light">
+          <h2>Correct/Wrong Audio:</h2>
+        </ion-item-divider>
+          <ion-item>
+            Correct Audio: {{content['answer_correct_audio']}}
+          </ion-item>
+          <ion-item>
+            Wrong Audio: {{content['answer_wrong_audio']}}
+          </ion-item>
+
+        <ion-item-divider color="light">
+          <h2>Choices Audio:</h2>
+        </ion-item-divider>
+          <ion-item>
+            Choice 1: {{content['choice_1_audio']}}
+          </ion-item>
+          <ion-item>
+            Choice 2: {{content['choice_2_audio']}}
+          </ion-item>
+          <ion-item>
+            Choice 3: {{content['choice_3_audio']}}
+          </ion-item>
+          <ion-item>
+            Choice 4: {{content['choice_4_audio']}}
+          </ion-item>
+      </ion-item-group>
+
     </ion-content>
   `
 })
 export class DebugController {
 
+  private debugInterface = 'menu';
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DebugPage');
+  }
+
+  private route(id){
+    console.log(id);
   }
 
 }
