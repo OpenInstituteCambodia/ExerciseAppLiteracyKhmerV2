5700173e71bb690590bd1b67c1c3b559d53c8eca
Socheat Sok <socheatsok78@gmail.com>
2017-03-17 16:54:56 +0700

    remove old app

--- app/src/app/main.ts
+++
@@ -1,9 +0,0 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-
-// Angular 2 is running in the development mode. Call enableProdMode() to enable the production mode.
-enableProdMode();
-
-platformBrowserDynamic().bootstrapModule(AppModule);
